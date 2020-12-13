accc24d78d296b8fa0f41be10960717a4dba358c
Stefan Svärd <stefan.nigma@gmail.com>
2020-12-13 14:22:07 +0100

    Add 3.1 project

---
+++ 3.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] logFile = File.ReadAllLines("input/input.txt");
+
+        }
+    }
+}
