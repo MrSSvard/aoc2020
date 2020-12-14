0371d2bf730b1c6b154a9ad4c9bf3626c629718f
Stefan Svärd <stefan@snok.standersson.se>
2020-12-14 22:14:54 +0100

    Solved 3.1

--- 3.1/Program.cs
+++ 3.1/Program.cs
@@ -8,8 +8,19 @@ namespace _3._1
     {
         static void Main(string[] args)
         {
-            string[] logFile = File.ReadAllLines("input/input.txt");
+            string[] map = File.ReadAllLines("input/input.txt");
+            var mapWidth = map[0].Length;
 
+            var treeCount = 0;
+            for (var row = 0; row < map.Length; row++)
+            {
+                var column = (row * 3) % mapWidth;
+                if (map[row][column] == '#')
+                {
+                    treeCount++;
+                }
+            }
+            Console.Write(treeCount);
         }
     }
 }
